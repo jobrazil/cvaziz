604d4b0ca4bd58d4ab0e665a59a28ce86521846d
Jo <joaquimdieudo@gmail.com>
2023-11-29 13:40:23 +0100

    deploiement à l'aide de netlify a l'adresse azize.be modifs mineures

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
       },
+      backgroundImage: {
+        'fond-div': "url('../img/so-white.png')",
+      },
       colors: {
         custom: {
           50: '#a73f2d',
